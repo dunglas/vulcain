db28a884ec3801047a3653e69e303f5c5641a202
Kévin Dunglas <dunglas@gmail.com>
2020-09-22 20:22:41 +0200

    tests: compat with old PHP versions

--- test-push/push-limit.php
+++ test-push/push-limit.php
@@ -10,4 +10,8 @@ require __DIR__ . '/tester.php';
 
 assertRequests([
     ['/books.jsonld', ['headers' => ['Preload' => '"/hydra:member/*/author"']]],
-], fn (string $logs): int => ($nb = preg_match_all('/Queueing pushed response/', $logs) === 2) ? 0 : $nb);
+], function (string $logs): int {
+    $nb = preg_match_all('/Queueing pushed response/', $logs);
+    
+    return $nb == 2 ? 0 : $nb;
+});
