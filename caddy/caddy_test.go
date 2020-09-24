859ee1de562b1f023b1c040599441f7e0d673397
Kévin Dunglas <dunglas@gmail.com>
2020-09-24 11:36:22 +0200

    feat: add a Caddy module

---
+++ caddy/caddy_test.go
@@ -0,0 +1,34 @@
+package caddy
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/caddyserver/caddy/v2/caddytest"
+)
+
+func TestVulcain(t *testing.T) {
+	tester := caddytest.NewTester(t)
+	tester.InitServer(` 
+	{
+	  order vulcain before request_header
+
+	  http_port     9080
+	  https_port    9443
+	}
+
+	localhost:9080 {
+	  file_server {
+	  	root fixtures
+	  }
+	  header /books* Content-Type application/ld+json
+	  vulcain
+	}`, "caddyfile")
+
+	resp, _ := tester.AssertGetResponse(`http://localhost:9080/books.jsonld?preload="/hydra:member/*"&fields="/hydra:member/*","/foo/0/bar/*/a"`, 200, `{"hydra:member":["/books-1.jsonld"],"foo":[{"bar":[{"a":"b"},{}]}]}`)
+
+	// Unfortunately, Go's HTTP client doesn't support Pushes yet (https://github.com/golang/go/issues/18594), so we test the fallback
+	if !reflect.DeepEqual(resp.Header["Link"], []string{"</books-1.jsonld>; rel=preload; as=fetch"}) {
+		t.Errorf("missing link header")
+	}
+}
--- go.mod
+++ go.mod
@@ -3,17 +3,22 @@ module github.com/dunglas/vulcain
 go 1.15
 
 require (
+	cloud.google.com/go v0.54.0 // indirect
+	github.com/caddyserver/caddy/v2 v2.2.0-rc.3.0.20200923021034-f197cec7f3a5
 	github.com/dunglas/httpsfv v0.1.0
 	github.com/getkin/kin-openapi v0.22.0
 	github.com/gofrs/uuid v3.3.0+incompatible
 	github.com/gorilla/handlers v1.5.1
 	github.com/joho/godotenv v1.3.0
+	github.com/klauspost/cpuid v1.3.0 // indirect
 	github.com/stretchr/testify v1.5.1
 	github.com/tidwall/gjson v1.6.1
 	github.com/tidwall/sjson v1.1.1
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.16.0
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
-	golang.org/x/net v0.0.0-20200904194848-62affa334b73
+	golang.org/x/net v0.0.0-20200923182212-328152dc79b1
 	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	howett.net/plist v0.0.0-20200419221736-3b63eb3a43b5 // indirect
 )
