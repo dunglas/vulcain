097a2c3e3e0fa5e35ec5607868108b24283ebd15
Kévin Dunglas <kevin@dunglas.fr>
2023-12-21 00:27:16 +0100

    chore: sync automaxprocs logging with upstream Caddy

--- caddy/vulcain/main.go
+++ caddy/vulcain/main.go
@@ -5,18 +5,19 @@ import (
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
 
 	"go.uber.org/automaxprocs/maxprocs"
+	"go.uber.org/zap"
 
 	// plug in Caddy modules here.
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
 	_ "github.com/dunglas/vulcain/caddy"
 )
 
-//nolint:gochecknoinits
-func init() {
-	//nolint:errcheck
-	maxprocs.Set(maxprocs.Logger(caddy.Log().Sugar().Debugf))
-}
-
 func main() {
+	undo, err := maxprocs.Set()
+	defer undo()
+	if err != nil {
+		caddy.Log().Warn("failed to set GOMAXPROCS", zap.Error(err))
+	}
+
 	caddycmd.Main()
 }
